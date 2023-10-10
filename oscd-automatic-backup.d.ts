4197944434d5b3334385d25fb18fb07c0baf1719
danyill <danyill@users.noreply.github.com>
2023-10-10 22:33:06 +0000

    Deploying to gh-pages from @ danyill/oscd-automatic-backup@b6a56a6d08a5b7d7553eb6df6a648bfb34ae6ee7 🚀

---
+++ oscd-automatic-backup.d.ts
@@ -0,0 +1,40 @@
+import { LitElement, TemplateResult } from 'lit';
+import '@material/mwc-button';
+import '@material/mwc-dialog';
+import '@material/mwc-formfield';
+import '@material/mwc-snackbar';
+import '@material/mwc-switch';
+import '@material/mwc-textfield';
+import type { Dialog } from '@material/mwc-dialog';
+import type { Snackbar } from '@material/mwc-snackbar';
+import type { Switch } from '@material/mwc-switch';
+import type { TextField } from '@material/mwc-textfield';
+export default class AutomaticBackup extends LitElement {
+    /** The document being edited as provided to plugins by [[`OpenSCD`]]. */
+    doc: XMLDocument;
+    docname: string;
+    usedDirectory: string;
+    usedFileNames: string[];
+    dialogUI?: Dialog;
+    enabledUI?: Switch;
+    intervalUI?: TextField;
+    countUI?: TextField;
+    messageActiveUI?: Snackbar;
+    messageBackupTakenUI?: Snackbar;
+    messageNotSupportedUI?: Snackbar;
+    diskUsageUI?: HTMLElement;
+    timerId: number;
+    docByteSize: number;
+    cancelDialog: boolean;
+    set enabled(state: boolean);
+    get enabled(): boolean;
+    set interval(interval: number);
+    get interval(): number;
+    set count(count: number);
+    get count(): number;
+    run(): Promise<void>;
+    calculateUsage(): void;
+    protected firstUpdated(): void;
+    render(): TemplateResult;
+    static styles: import("lit").CSSResult;
+}
