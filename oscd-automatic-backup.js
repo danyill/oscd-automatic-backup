17577f46dde487eb4dba0a7f6316d82df0dd0684
danyill <danyill@users.noreply.github.com>
2023-10-14 13:56:47 +0000

    Deploying to gh-pages from @ danyill/oscd-automatic-backup@80f08e08d4904b5afe107643fdae4570c856c123 🚀

--- oscd-automatic-backup.js
+++ oscd-automatic-backup.js
@@ -6531,7 +6531,7 @@ class AutomaticBackup extends s$2 {
         window.addEventListener('oscd-edit', () => {
             if (this.usedDirectory === '' && this.enabled) {
                 this.userMessage = `You have automatic backups enabled but a directory has not been selected, 
-        please either disable or choose a directory by going to Menu &gt; Automatic Backups.`;
+        please either disable or choose a directory by going to Automatic Backups in the Menu.`;
                 if (this.userMessageUI)
                     this.userMessageUI.show();
             }
